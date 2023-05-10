7255d5b489f770813096c145118f03ab9bbe9bfb
Caleb <cheruiyotdanielcaleb@gmail.com>
2023-05-10 12:36:15 +0300

    initial commit

---
+++ intro.py
@@ -0,0 +1,12 @@
+# This basically the introduction
+
+# Data types, Variables, Print function
+print("This is my first program with Python")
+
+# Variables with different data types
+character_name = "Caleb"    # String Data type
+character_age = 20   # Integer data type
+is_male = False  # Boolean Data type
+
+print("Well Well Well, if it isn't the guy himself "+ character_name + "!")
+print("He is ", str(character_age), "years old by the time of this publishing.")
